c13f60685b310737d23e3d8d025e461edc645333
LilBoooopp <cbopp@student.42lausanne.ch>
2026-01-25 18:32:17 +0100

    mariadb script

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -19,7 +19,7 @@ if [ ! -d "/var/lib/mysql/${SQL_DATABASE}" ]; then
   mariadb -e "CREATE DATABASE IF NOT EXISTS \'${SQL_DATABASE}\`;"
   echo "Creating user: $SQL_USER"
   mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-  mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+  mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';"
   echo "securing root user"
   mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
   mariadb -e "FLUSH PRIVILEGES;"
