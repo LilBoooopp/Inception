44df4dccc54342da460464885efbb9467eecdf51
LilBoooopp <cbopp@student.42lausanne.ch>
2026-01-25 18:24:05 +0100

    mariadb verbose script

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -6,17 +6,25 @@ SQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 if [ ! -d "/var/lib/mysql/${SQL_DATABASE}" ]; then
   echo "Database not found. Initializing..."
 
+  echo "-- STARTING MARIADB SETUP --"
   service mariadb start
 
+  echo "Waiting for Mariadb..."
   while ! mysqladmin ping -hlocalhost --silent; do
     sleep 1
   done
+  echo "Mariadb UP"
 
+  echo "Creating database: $SQL_DATABASE"
   mariadb -e "CREATE DATABASE IF NOT EXISTS \'${SQL_DATABASE}\`;"
+  echo "Creating user: $SQL_USER"
   mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
   mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+  echo "securing root user"
   mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
   mariadb -e "FLUSH PRIVILEGES;"
+
+  echo "-- SETPU FINISHED. RESTARTING --"
 
   mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
   echo "Database initialized."
