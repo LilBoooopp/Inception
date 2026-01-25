958a696d756b47068e2511a31e19520886318aa0
LilBoooopp <cbopp@student.42lausanne.ch>
2026-01-25 18:01:47 +0100

    automate wp config

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
-SQL_PASSWORD=$(cat /run/secrets/db_password)
-WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
+mkdir -p /run/php
 
-mkdir -p /var/www/wordpress
+sleep 10
 
 cd /var/www/wordpress
 
+# check if wp is there
 if [ ! -f wp-config.php ]; then
+  # if the folder is empty, download wp again
+  if [ ! -f index.php ]; then
+    wp core download --allow-root
+  fi
+
+  SQL_PASSWORD=$(cat /run/secrets/db_password)
+  WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
+
+  # configure connection
   wp config create  --allow-root \
                     --dbname=$SQL_DATABASE \
                     --dbuser=$SQL_USER \
                     --dbpass=$SQL_PASSWORD \
-                    --dbpass=$SQL_HOST \
+                    --dbhost=$SQL_HOST \
                     --path='/var/www/wordpress'
   
+  # install site
   wp core install --allow-root \
                   --url=$WP_URL \
                   --title=$WP_TITLE \
@@ -23,6 +33,7 @@ if [ ! -f wp-config.php ]; then
                   --admin_email=$WP_ADMIN_EMAIL \
                   --path='/var/www/wordpress'
 
+  # create user
   wp user create  --allow-root \
                   $WP_USER $WP_EMAIL \
                   --user_pass=$WP_PASSWORD \
@@ -30,7 +41,5 @@ if [ ! -f wp-config.php ]; then
                   --path='/var/www/wordpress'
 
 fi
-
-mkdir -p /run/php
 
 exec /usr/sbin/php-fpm8.2 -F
