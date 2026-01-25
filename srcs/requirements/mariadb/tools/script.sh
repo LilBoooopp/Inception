b7ab6ab825ac993d84b9d48cec6c98e39f0d8c80
LilBoooopp <cbopp@student.42lausanne.ch>
2026-01-25 18:15:03 +0100

    mariadb script typos

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -3,7 +3,7 @@
 SQL_PASSWORD=$(cat /run/secrets/db_password)
 SQL_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
-if [ ! -d "/var/liv/mysql/${SQL_DATABASE}" ]; then
+if [ ! -d "/var/lib/mysql/${SQL_DATABASE}" ]; then
   echo "Database not found. Initializing..."
 
   service mariadb start
@@ -12,8 +12,8 @@ if [ ! -d "/var/liv/mysql/${SQL_DATABASE}" ]; then
     sleep 1
   done
 
-  mariadb -e "CREATE DATABASE IF NOT EXIST \'${SQL_DATABASE}\`;"
-  mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+  mariadb -e "CREATE DATABASE IF NOT EXISTS \'${SQL_DATABASE}\`;"
+  mariadb -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
   mariadb -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
   mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
   mariadb -e "FLUSH PRIVILEGES;"
