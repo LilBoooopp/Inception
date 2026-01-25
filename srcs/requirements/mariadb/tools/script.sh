1e8bfdf0ae26c337dbd36f706e3fe9395c9b7b56
LilBoooopp <cbopp@student.42lausanne.ch>
2026-01-25 18:38:43 +0100

    flushing before changing root

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -19,10 +19,11 @@ if [ ! -d "/var/lib/mysql/${SQL_DATABASE}" ]; then
   mariadb -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE;"
   echo "Creating user: $SQL_USER"
   mariadb -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD';"
+  echo "Granting privileges"
   mariadb -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%';"
+  mariadb -e "FLUSH PRIVILEGES;"
   echo "securing root user"
   mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
-  mariadb -e "FLUSH PRIVILEGES;"
 
   echo "-- SETPU FINISHED. RESTARTING --"
 
