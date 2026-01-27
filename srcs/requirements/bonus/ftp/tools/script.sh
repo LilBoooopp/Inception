873ce4d1876e3d855f5563b500a199e4a448cd5f
LilBoooopp <cbopp@student.42lausanne.ch>
2026-01-27 18:09:45 +0100

    ftp home correction

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -2,13 +2,13 @@
 
 FTP_PASSWORD=$(cat /run/secrets/ftp_password)
 
-adduser --disabled-password --gecos "" $FTP_USER
+adduser --disabled-password --gecos "" --home /var/www/wordpress --no-create-home $FTP_USER
 echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
 echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
 
-mkdir -p /var/www/wordpress
-chown -R $FTP_USER:$FTP_USER /var/www/wordpress
+mkdir -p /var/www/wordpress/
+chown -R $FTP_USER:$FTP_USER /var/www/wordpress/
 
 mkdir -p /var/run/vsftpd/empty
 
